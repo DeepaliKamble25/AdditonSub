4da18ef9fc853149308ec0dc8155833962535d63
DeepaliKamble25 <kambledeepali25@gmail.com>
2023-04-24 19:50:35 +0530

    this is done by deepali

---
+++ src/main/java/com/add/AdditionLogic/Calculator/Sum.java
@@ -0,0 +1,16 @@
+package com.add.AdditionLogic.Calculator;
+
+public class Sum {
+	
+	
+	public int Sum(int a, int b) {
+		
+		int c=a+b;
+		
+		System.out.println("Addition done with Calculator used  :"+c);
+		
+		return c;
+		
+	}
+
+}
